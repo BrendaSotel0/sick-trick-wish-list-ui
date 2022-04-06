9569fea5cced8c6761def48c0ee74bac8459e57c
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-06 12:11:27 -0600

    Times up

--- src/components/App/TrickCard.js
+++ src/components/App/TrickCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 const TrickCard = ({stance, name, obstacle, tutorial, id}) => {
   return (
     <div className='card'>
+      <h2>Stance: {stance}</h2>
       <h2>{name}</h2>
       <h2>Obstacle: {obstacle}</h2>
       <h2>Link to tutorial: <a href='{tutorial}'>{tutorial}</a></h2>

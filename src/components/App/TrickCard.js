b27c694ecc0ac7139aef1277e18ded34fe182c90
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-06 11:10:56 -0600

    Display tricks

--- src/components/App/TrickCard.js
+++ src/components/App/TrickCard.js
@@ -3,9 +3,9 @@ import React from 'react';
 const TrickCard = ({stance, name, obstacle, tutorial, id}) => {
   return (
     <div className='card'>
-      <h2>Name: {name}</h2>
+      <h2>{name}</h2>
       <h2>Obstacle: {obstacle}</h2>
-      <h2>Link to tutorial: {tutorial}</h2>
+      <h2>Link to tutorial: <a href='{tutorial}'>{tutorial}</a></h2>
     </div>
   )
 }
